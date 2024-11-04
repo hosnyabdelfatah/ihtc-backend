742a4ab5e6833d40722b8906999e10e5cb17560f
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2024-11-04 20:45:39 +0200

    Update Cors setup

--- app.js
+++ app.js
@@ -24,18 +24,19 @@ app.use(express.static(path.join(__dirname, `public`)));
 const allowedOrigins = [
     "http://localhost:3000",
     "https://ihtc.vercel.app",
-    "https://ihtc-q2teign6e-hosnyabdelfatahs-projects.vercel.app/"
+    "https://ihtc-q2teign6e-hosnyabdelfatahs-projects.vercel.app"
 ];
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
+    // origin: (origin, callback) => {
+    //     if (allowedOrigins.includes(origin) || !origin) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error('Not allowed by CORS'));
+    //     }
+    // },
+    origin: '*',
+    credentials: true,// Important for cookies/credentials
     exposedHeaders: ['Set-Cookie', 'Date', 'ETag'],
     optionsSuccessStatus: 200
 };
@@ -49,6 +50,24 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true})); //Review it
 app.use(express.json({limit: "10kb"}));
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+    res.setHeader('Referrer-Policy', 'no-referrer-when-downgrade'); // Adjust as needed
+    next();
+});
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://your-frontend-origin.com'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies/credentials
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin'); // Set referrer policy
+    next();
+});
+
+
+app.use((req, res, next) => {
+    console.log('Request Origin:', req.get('Origin'));
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello in IHTC World!');
