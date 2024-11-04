0b7a9e17dcd394999da70668f66c53d9e5b40c94
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2024-11-04 17:55:29 +0200

    change server.js to index.js and install parcel-bundler

--- dist/index.53bf2009.js
+++
@@ -1,2 +0,0 @@
-console.log("Parcel build test!"),document.getElementById("app").innerText="Hello from Parcel!";
-//# sourceMappingURL=index.53bf2009.js.map
--- dist/index.e7d732e6.js
+++
@@ -1,2 +0,0 @@
-console.log("Parcel build test!");
-//# sourceMappingURL=index.e7d732e6.js.map
