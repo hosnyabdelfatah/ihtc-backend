79647d49c1f2948c66a0c807c6a159b2377994e7
hosnyabdelfatah <hosnyabdelfatah@gmail.com>
2024-11-04 22:47:09 +0200

    Add cros configuration to vercel.json and update app.js file to reponse headers

--- app.js
+++ app.js
@@ -51,9 +51,14 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://your-frontend-origin.com'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your frontend URL
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies/credentials
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin'); // Set referrer policy
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     next();
 });
 
